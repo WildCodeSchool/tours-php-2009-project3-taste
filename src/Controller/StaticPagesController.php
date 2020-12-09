2e00562c4e771ff523e78eb99ad37cb555dca4ac
axelhamil <axel.hamil11@gmail.com>
2020-12-09 16:27:27 +0100

    change ViewController with StaticPagesControllere

--- src/Controller/StaticPagesController.php
+++ src/Controller/StaticPagesController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ViewController extends AbstractController
+class StaticPagesController extends AbstractController
 {
     /**
      * @Route("/", name="index")

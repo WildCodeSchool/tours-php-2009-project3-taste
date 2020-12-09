8f1e95ae712b336e957d08fe29e9cccdbde78633
axelhamil <axel.hamil11@gmail.com>
2020-12-09 13:50:46 +0100

    rename controller

--- src/Controller/ViewController.php
+++ src/Controller/ViewController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {

9034f5439aec79b8be25d02f36a260ba1fa63d50
axelhamil <axel.hamil11@gmail.com>
2020-12-10 18:28:16 +0100

    I added some comments in my code

--- src/Controller/StaticPagesController.php
+++ src/Controller/StaticPagesController.php
@@ -6,9 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * This class is used to display all static views
+ * @package App\Controller
+ */
 class StaticPagesController extends AbstractController
 {
     /**
+     * This method is used to display the home page
+     * @return Response
      * @Route("/", name="index")
      */
     public function index(): Response

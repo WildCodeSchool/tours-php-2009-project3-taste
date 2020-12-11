bae039bdc7bc2170a560abfb936c815c617bc8b3
Mgaren <basketeur009@gmail.com>
2020-12-11 11:23:45 +0100

    mise a jour de la user FAQ

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,21 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+    /**
+     * @Route("/FAQ", name="FAQ")
+     * @return Response
+     */
+    public function pageF(): Response
+    {
+        return $this->render('FAQ-CGU/faq.html.twig');
+    }
+
+    /**
+     * @Route("/CGU", name="CGU")
+     * @return Response
+     */
+    public function pageC(): Response
+    {
+        return $this->render('FAQ-CGU/cgu.html.twig');
+    }
 }

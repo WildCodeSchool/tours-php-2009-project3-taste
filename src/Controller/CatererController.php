491d7deaa873c6687e9bd30005320e5769fdc206
Maxime <auneau.maxime@gmail.com>
2020-12-16 13:02:19 +0100

    Adding redirection

--- src/Controller/CatererController.php
+++ src/Controller/CatererController.php
@@ -6,18 +6,27 @@ use App\Entity\Caterer;
 use App\Form\CatererType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CatererController extends AbstractController
 {
     /**
      * @Route("/caterer", name="caterer")
+     * @param Request $request
      * @return Response
      */
-    public function catererPage(): Response
+    public function catererPage(Request $request): Response
     {
         $caterer = new Caterer();
         $form = $this->createForm(CatererType::class, $caterer);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($caterer);
+            $entityManager->flush();
+            return $this->redirectToRoute('index');
+        }
         return $this->render('caterer/caterer.html.twig', ["form" => $form->createView()]);
     }
 }

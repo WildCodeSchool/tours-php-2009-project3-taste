809e6e599723520e09d6599c27434c78551b3719
emrecaydin <emrecaydin00@gmail.com>
2021-01-12 10:03:20 +0100

    modifications

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,5 @@ import '../styles/app.scss';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
 // create global $ and jQuery variables
+global.$ = $;
+global.jQuery = $;

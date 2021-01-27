6ae45a7134e9c034e9aef983c5f0dafdb7b24a5f
Mgaren <basketeur009@gmail.com>
2021-01-27 09:37:31 +0100

    formulaire d'inscription MAJ

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository", repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Ce mail est déjà utilisé !")
  */
 class User implements UserInterface
 {
